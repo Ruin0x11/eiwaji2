b28138ad55df676cd755a1250b99e3257fa22798
Ruin0x11 <ipickering2@gmail.com>
2021-09-16 14:15:30 -0700

    Update link to JMdict_e.gz

--- build_db.bat
+++ build_db.bat
@@ -4,7 +4,7 @@ set PATH=%cd%\lib;%PATH%
 
 pushd tools
 if not exist JMdict_e.gz (
-    powershell -Command "Invoke-WebRequest ftp://ftp.monash.edu.au/pub/nihongo/JMdict_e.gz -OutFile JMdict_e.gz"
+    powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz -OutFile JMdict_e.gz"
 )
 if not exist JMdict_e (
     powershell -Command "../lib/7z.exe x JMdict_e.gz"
